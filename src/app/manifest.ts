09c30580f4f80c9441ea55d10816f01b292454fc
Ken Sørevåge <ken.paulsen@gmail.com>
2025-11-21 08:48:32 +0100

    feat: update app icons and manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,26 +15,26 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ['productivity', 'utilities', 'lifestyle'],
     icons: [
       {
-        src: '/icons/icon-192x192.svg',
+        src: '/icons/app-icon.png',
         sizes: '192x192',
-        type: 'image/svg+xml',
+        type: 'image/png',
         purpose: 'maskable'
       },
       {
-        src: '/icons/icon-512x512.svg',
+        src: '/icons/app-icon.png',
         sizes: '512x512',
-        type: 'image/svg+xml', 
+        type: 'image/png',
         purpose: 'maskable'
       },
       {
-        src: '/favicon.ico',
-        sizes: '48x48',
-        type: 'image/x-icon'
+        src: '/icon',
+        sizes: 'any',
+        type: 'image/png'
       },
       {
-        src: '/icons/apple-touch-icon.svg',
+        src: '/apple-icon',
         sizes: '180x180',
-        type: 'image/svg+xml'
+        type: 'image/png'
       }
     ]
   }
